39c7ae657ab4e2735bd38103a978bf3b34607e6a
CamiloCastiblanco <69698380+CamiloCastiblanco@users.noreply.github.com>
2023-02-22 23:10:16 -0500

    Update BlueprintsServices.java

--- src/main/java/edu/eci/arsw/blueprints/services/BlueprintsServices.java
+++ src/main/java/edu/eci/arsw/blueprints/services/BlueprintsServices.java
@@ -9,9 +9,13 @@ import edu.eci.arsw.blueprints.model.Blueprint;
 import edu.eci.arsw.blueprints.model.Point;
 import edu.eci.arsw.blueprints.persistence.BlueprintNotFoundException;
 import edu.eci.arsw.blueprints.persistence.BlueprintsPersistence;
+
+import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Set;
+
+import edu.eci.arsw.blueprints.persistence.Filter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,29 +25,49 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class BlueprintsServices {
-   
+
     @Autowired
-    BlueprintsPersistence bpp=null;
-    
-    public void addNewBlueprint(Blueprint bp){
-        
+    BlueprintsPersistence bpp = null;
+
+    @Autowired
+    Filter filter;
+
+    public void addNewBlueprint(Blueprint bp) {
+        try {
+            bpp.saveBlueprint(bp);
+        } catch (Exception e) {
+            throw new UnsupportedOperationException("Error with the operation on services.");
+        }
     }
-    
-    public Set<Blueprint> getAllBlueprints(){
-        return null;
+
+    public Set<Blueprint> getAllBlueprints() {
+        Set<Blueprint> blueprints;
+        try {
+            blueprints = bpp.getAllBlueprints();
+        } catch (Exception e) {
+            throw new UnsupportedOperationException("Error with the operation on services.");
+        }
+        return blueprints;
     }
-    
+
     /**
      * 
      * @param author blueprint's author
-     * @param name blueprint's name
+     * @param name   blueprint's name
      * @return the blueprint of the given name created by the given author
      * @throws BlueprintNotFoundException if there is no such blueprint
      */
-    public Blueprint getBlueprint(String author,String name) throws BlueprintNotFoundException{
-        throw new UnsupportedOperationException("Not supported yet."); 
+    public Blueprint getBlueprint(String author, String name) throws BlueprintNotFoundException {
+        Blueprint blueprint;
+        try {
+            blueprint = bpp.getBlueprint(author, name);
+            blueprint =  filter.filterPoints(blueprint);
+        } catch (Exception e) {
+            throw new UnsupportedOperationException("Error with the operation on services.");
+        }
+        return blueprint;
     }
-    
+
     /**
      * 
      * @param author blueprint's author
@@ -51,7 +75,14 @@ public class BlueprintsServices {
      * @throws BlueprintNotFoundException if the given author doesn't exist
      */
     public Set<Blueprint> getBlueprintsByAuthor(String author) throws BlueprintNotFoundException{
-        throw new UnsupportedOperationException("Not supported yet."); 
+        Set<Blueprint> blueprints = bpp.getBlueprintsByAuthor(author);
+        Set<Blueprint> blueprintsWithFilter = new HashSet<>();
+        for(Blueprint bp: blueprints){
+            bp = filter.filterPoints(bp);
+            blueprintsWithFilter.add(bp);
+        }
+        return blueprintsWithFilter;
     }
-    
+
 }
+
